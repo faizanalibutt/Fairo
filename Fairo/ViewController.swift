592adb0055140179787e3cec8b747fade7f5b0ad
Faizi <mfaizanalibutt@gmail.com>
2021-10-14 16:09:33 +0500

    first commit henry project

--- Fairo/ViewController.swift
+++ Fairo/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var fairoText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let scanSuccessText =
+            "Your card has been scanned successfully." +
+        "\n\nWe will now capture a selfie to verify your identity. When you’re ready, press the button below."
+        fairoText.text = scanSuccessText
     }
-
-
+    
+    @IBAction func closeFairo(_ sender: UIButton) {
+        print("Selfie button clicked.")
+        let henryHooks = "henrycards://"
+        let henryUrl = URL(string: henryHooks)!
+        UIApplication.shared.open(henryUrl)
+    }
+    
 }
 
